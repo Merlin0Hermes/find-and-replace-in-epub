1ab5c2423fe5a7e739561ead21c52b70494418d5
Merlin Hermes <166232409+Merlin0Hermes@users.noreply.github.com>
2025-04-30 17:10:35 +0530

    move useful functions to utils.py

---
+++ utils.py
@@ -0,0 +1,24 @@
+import os
+import csv
+import platform
+from pathlib import Path
+
+def make_directory(dir_name):
+    try:
+        Path(dir_name).mkdir(parents=True, exist_ok=True)
+        return Path(dir_name)
+    except Exception as e:
+        print(f"An error occured")
+        
+
+def clear_terminal():
+    if platform.system == "Windows":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+
+def dict_from_csv(filename):
+    with open(filename, "r") as file:
+        reader = csv.DictReader(file)
+        return {row["find"]: row["replace"] for row in reader}
